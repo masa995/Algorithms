5dac08dd872e74bcaa6147b3541198685977f37b
Mariya <masa995@yandex.ru>
2022-03-10 19:22:57 +0300

    update quick sort

--- Grokking-algorithms/Quick-sort/script.js
+++ Grokking-algorithms/Quick-sort/script.js
@@ -1,13 +1,17 @@
-function quickSort(arr) {
-  if (arr.length < 2) return arr;
+function quickSort(array) {
+  let arr = array.slice();  //array immutable
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-  const pivot = arr[Math.floor(Math.random() * arr.length)],
-    less = arr.filter(el => el < pivot),
-    equal = arr.filter(el => el === pivot),
-    greater = arr.filter(el => el > pivot);
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const less = arr.filter(el => el < pivot);
+  const middle = arr.filter(el => el === pivot);
+  const more = arr.filter(el => el > pivot);
 
-  return [...quickSort(less), ...equal, ...quickSort(greater)];
+  return [...quickSort(less), ...middle, ...quickSort(more)]; //Spread  
 }
 
-const nums = [0, 2, 2, 1, 5, 3, 89];
+const nums = [1, 3, 0, 1, 55, 9];
 console.log(quickSort(nums));
+console.log('Array nums ' + nums);

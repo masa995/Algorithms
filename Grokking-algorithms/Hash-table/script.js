6a4350af3c54d4cd479d07527b369895b8928058
Mariya <masa995@yandex.ru>
2022-03-11 21:08:22 +0300

    add hash function in folder Hash-table

--- Grokking-algorithms/Hash-table/script.js
+++ Grokking-algorithms/Hash-table/script.js
@@ -1,3 +1,31 @@
-function hashTable() {
+class hashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
 
+  /**Назначение простой хэш-функции:
+  
+    *прием значения;
+  
+    *выполнение циклов через каждый символ;
+  
+    *возвращение кода символа для каждого символа;
+  
+    *умножение его на индекс;
+  
+    *добавление его к исходному хэш-значению;
+  
+    *использование оператора остатка для непревышения длины размера массива.
+  */
+
+  hashFunction(value) {
+    let hash = 0;
+    for (let i = 0; i < value.length; i++) {
+      hash = (hash + value.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
 }
+
+const elem = new hashTable(4);
+console.log(elem.hashFunction('Hey'));

a8fee7f6a6e0260fe29aedfe2d95fb2a86f03a4b
Mariya <masa995@yandex.ru>
2022-03-15 19:49:52 +0300

    make an array immutable in a folder Insertion-sort

--- Other-algorithms/Insertion-sort/script.js
+++ Other-algorithms/Insertion-sort/script.js
@@ -1,11 +1,9 @@
-function insertionSort(arr) {
-  let n = arr.length;
-  for (let i = 1; i < n; i++) {
-    // Choosing the first element in our unsorted subarray
+function insertionSort(array) {
+  let arr = array.slice();  //array immutable
+  for (let i = 1; i < arr.length; i++) {
     let current = arr[i];
-    // The last element of our sorted subarray
     let j = i - 1;
-    while ((j > -1) && (current < arr[j])) {
+    while ((j >= 0) && (current < arr[j])) {
       arr[j + 1] = arr[j];
       j--;
     }
@@ -14,6 +12,7 @@ function insertionSort(arr) {
   return arr;
 }
 
-let arr = [5, 2, 4, 6, 1, 3];
-insertionSort(arr);
+let arr = [5, 6, 4, 6, 1, 3];
 console.log(arr);
+let result = insertionSort(arr);
+console.log(result);

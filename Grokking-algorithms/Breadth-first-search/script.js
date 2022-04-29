de679b205881bcb5e4fd9335b49f00affe1987ca
Mariya <masa995@yandex.ru>
2022-04-29 18:53:54 +0300

    add Adjacency matrix, Adjacency list and update Breadth first search

--- Grokking-algorithms/Breadth-first-search/script.js
+++ Grokking-algorithms/Breadth-first-search/script.js
@@ -1,41 +1,116 @@
-// при условии, что мы имеем дело со смежным списком
-// например, таким: adj = {A:[B,C,D], B:[E,F], ... }
-function bfs(adj, s, t) {
-  // adj - смежный список
-  // s - начальная вершина
-  // t - пункт назначения
+const nodes = [0, 1, 2, 3];
+const edges = [
+  [0, 1],
+  [1, 2],
+  [1, 3],
+  [3, 0],
+  [3, 2]
+];
 
-  // инициализируем очередь
-  let queue = []
-  // добавляем s в очередь
-  queue.push(s)
-  // помечаем s как посещенную вершину во избежание повторного добавления в очередь
-  s.visited = true
-  while (queue.length > 0) {
-    // удаляем первый (верхний) элемент из очереди
-    let v = queue.shift()
-    // abj[v] - соседи v
-    for (let neighbor of adj[v]) {
-      // если сосед не посещался
-      if (!neighbor.visited) {
-        // добавляем его в очередь
-        queue.push(neighbor)
-        // помечаем вершину как посещенную
-        neighbor.visited = true
-        // если сосед является пунктом назначения, мы победили
-        if (neighbor === t) return true
+const nodesA = ['A', 'B', 'C', 'D', 'F', 'E', 'G', 'H', 'I'];
+const edgesA = [
+  ['A', 'B'],
+  ['A', 'C'],
+  ['A', 'D'],
+  ['B', 'E'],
+  ['B', 'F'],
+  ['C', 'G'],
+  ['D', 'H'],
+  ['D', 'I']
+];
+
+function getAdjacencyList(nodes, edges, unoriented) {
+  let list = {}
+  for (let i = 0; i < nodes.length; i++) {
+    let key = nodes[i]
+    list[key] = [];
+    for (let edge of edges) {
+      let [a, b] = edge;
+      if (a === key) {
+        list[key].push(b)
+      }
+      if (unoriented) {
+        if (b === key) {
+          list[key].push(a)
+        }
       }
     }
   }
-  // если t не обнаружено, значит пункта назначения достичь невозможно
-  return false
+  return list;
 }
 
-const adj = {
-  A: ['B', 'C', 'D'],
-  B: ['E', 'F'],
-  C: ['G'],
-  D: ['H', 'I']
-};
-console.log(bfs(adj, 'A', 'G'));
-console.log(bfs(adj, 'C', 'I'));
+function breadthFirstSearchList(list, startNode, endNode) {
+  let checked = [];
+  let queue = [startNode];
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+    checked.push(node);
+
+    for (item of list[node]) {
+      if (!checked.includes(item) && !queue.includes(item)) {
+
+        if (item === endNode) {
+          return true;
+        }
+        queue.push(item);
+      }
+    }
+
+  }
+  return false;
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+function getAdjacencyMatrix(nodes, edges, unoriented) {
+  let matrix = [];
+  for (let i = 0; i < nodes.length; i++) {
+    let row = [];
+
+    for (let i = 0; i < nodes.length; i++) {
+      row.push(0);
+    }
+    matrix.push(row);
+  }
+
+  for (let edge of edges) {
+    let [a, b] = edge;
+    matrix[a][b] = 1;
+    if (unoriented) {
+      matrix[b][a] = 1;
+    }
+  }
+  return matrix;
+}
+
+function breadthFirstSearchMatrix(matrix, startNode, endNode) {
+  let checked = [];
+  let queue = [startNode];
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+    checked.push(node);
+
+    for (let i = 0; i < matrix[node].length; i++) {
+      let neighbor = matrix[node][i];
+
+      if (neighbor && !checked.includes(i) && !queue.includes(i)) {
+        if (i === endNode) {
+          return true;
+        }
+        queue.push(i);
+      }
+    }
+  }
+  return false;
+}
+
+const list = getAdjacencyList(nodesA, edgesA, false);
+const matrix = getAdjacencyMatrix(nodes, edges, true);
+
+console.log(list);
+console.log(matrix);
+
+console.log(breadthFirstSearchList(list, 'A', 'I'));
+console.log(breadthFirstSearchMatrix(matrix, 2, 0));

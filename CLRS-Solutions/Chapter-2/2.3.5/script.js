344abf212bbe4805cb03a82cd37ef25a217b74d3
Mariya <masa995@yandex.ru>
2022-05-22 22:33:09 +0300

    add solution CLRS 2.3.5

---
+++ CLRS-Solutions/Chapter-2/2.3.5/script.js
@@ -0,0 +1,46 @@
+const arr = [1, 3, 6, 8, 9, 9, 9, 10];
+//const arr = [9, 30];
+//======================================================================================================================
+/**   РЕКУРСИВНЫЙ МЕТОД */
+const low = -1;
+const hight = arr.length;
+
+function binarySearchRecursive(arr, el, low, hight) {
+  let middle = Math.floor((low + hight) / 2);
+  if (hight - low === 1) {
+    return -1;
+  }
+
+  if (arr[middle] === el) {
+    return middle;
+  } else if (arr[middle] > el) {
+    return binarySearchRecursive(arr, el, low, middle)
+  } else if (arr[middle] < el) {
+    return binarySearchRecursive(arr, el, middle, hight)
+  }
+}
+
+//========================================================================================================================
+/**   ИТЕРАТИВНЫЙ МЕТОД*/
+function binarySearch(arr, el) {
+  let low = -1;
+  let hight = arr.length;
+  while (hight - low > 1) {
+    let middle = Math.floor((low + hight) / 2);
+    if (arr[middle] === el) {
+      return middle;
+    } else if (arr[middle] < el) {
+      low = middle;
+    } else if (arr[middle] > el) {
+      hight = middle;
+    }
+  }
+  return -1;
+}
+
+console.log(arr);
+console.log(binarySearch(arr, 6) + " binarySearch");
+console.log(binarySearch(arr, 30) + " binarySearch");
+
+console.log(binarySearchRecursive(arr, 9, low, hight) + " binarySearchRecursive");
+console.log(binarySearchRecursive(arr, 30, low, hight) + " binarySearchRecursive");

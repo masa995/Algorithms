0276b2deff6525cfa43adf0a85b97f1b8b0abf88
Mariya <masa995@yandex.ru>
2022-07-05 23:57:10 +0300

    update solution CLRS 9.2.3

--- CLRS-Solutions/Chapter-9/9.2.3/script.js
+++ CLRS-Solutions/Chapter-9/9.2.3/script.js
@@ -1,27 +1,55 @@
+function randomElem(min, max) {
+  let random = min + Math.random() * (max + 1 - min);
+  return Math.floor(random);
+}
+
+function randomPartition(arr, firstI, lastI) {
+  let pivot = randomElem(firstI, lastI);
+  [arr[lastI], arr[pivot]] = [arr[pivot], arr[lastI]]; //swap
+  return partition(arr, firstI, lastI);
+}
+
+function partition(arr, firstI, lastI) {
+  let x = arr[lastI];
+  let i = firstI - 1;
+
+  for (let j = firstI; j < lastI; j++) {
+    if (arr[j] <= x) {
+      i = i + 1;
+      [arr[i], arr[j]] = [arr[j], arr[i]]; //swap
+    }
+  }
+  [arr[i + 1], arr[lastI]] = [arr[lastI], arr[i + 1]]; // swap
+  return lastI;
+}
+
+/// Функция глючит =_=
+
 function randomSelectIterative(arr, i) {
-  let p = 0;
-  let r = arr.length - 1;
+  let firstI = 0;
+  let lastI = arr.length - 1;
 
-  while (r > p) {
-    let q = randomPartition();
-    let k = q - p + 1;
-
+  while (true) {
+    if (firstI === lastI) {
+      return arr[firstI];
+    }
+    let pivot = randomPartition(arr, firstI, lastI);
+    let k = pivot - firstI + 1;
     if (i === k) {
-      return arr[q]
-    } else if (i < k) {
-      r = q - 1;
+      return arr[pivot];
+    }
+
+    if (i < k) {
+      lastI = pivot - 1;
     } else {
-      p = q + 1;
+      firstI = pivot + 1;
       i = i - k;
     }
   }
-  return arr[p]
 }
 
-function randomPartition(arr, p, r) {
-  let x = Math.random(p - 1, r);
-  [arr[x], arr[r]] = [arr[r], arr[x]];
-  return partition(arr, p, r);
-}
-
-function partition(arr, p, r) { }
+let numR = [1, 2, 2, 3, 4];
+let num = [2, 1, 4, 2, 3];
+console.log(numR);
+console.log(num);
+console.log(randomSelectIterative(num, 4));

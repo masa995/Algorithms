b2a894e8cd1bc42fdc67b8dee80f264e2363fcac
Mariya <masa995@yandex.ru>
2022-08-08 18:28:57 +0300

    add solution CLRS 10.1.5

--- CLRS-Solutions/Chapter-10/10.1.5/script.js
+++ CLRS-Solutions/Chapter-10/10.1.5/script.js
@@ -1,52 +1,117 @@
-class Queue {
+class Deque {
   constructor() {
     this.arr = new Array(7);
-    this.head = 0;
-    this.tail = 1;
+    this.head;
+    this.tail;
+    this.left = 0;
+    this.right = 1;
   }
 
-  enqueue(x) {
-    if (this.head === this.tail) {
+  isFull() {
+    if (this.left === this.right) {
       console.error('Overflow');
-    }
-
-    this.arr[this.tail] = x;
-
-    if (this.tail === this.arr.length - 1) {
-      this.tail = 1;
-    } else {
-      this.tail += 1;
-    }
-    if (this.head === 0) {
-      this.head = 1;
+      return true
     }
   }
 
-  dequeue() {
-    if (this.head === 0) {
-      console.error('underflow');
+  isEmpty() {
+    if (this.left === this.tail || this.right === this.head || this.head === undefined) {
+      console.error('Underflow');
+      return true
     }
+  }
+
+  pushBack(x) {
+    if (this.isFull()) {
+      return
+    }
+
+    this.arr[this.right] = x;
+
+    if (this.right === this.arr.length - 1) {
+      this.tail = this.right;
+      this.right = 0;
+    } else {
+      this.tail = this.right;
+      this.right += 1
+    }
+
+    if (this.head === undefined) {
+      this.head = this.tail;
+    }
+  }
+
+  pushFront(x) {
+    if (this.isFull()) {
+      return
+    }
+
+    this.arr[this.left] = x;
+
+    if (this.left === 0) {
+      this.head = this.left;
+      this.left = this.arr.length - 1;
+
+    } else {
+      this.head = this.left;
+      this.left -= 1
+    }
+
+    if (this.tail === undefined) {
+      this.tail = this.head;
+    }
+  }
+
+  popBack() {
+    if (this.isEmpty()) {
+      return
+    }
+
+    let x = this.arr[this.tail];
+
+    if (this.right === 0) {
+      this.tail = this.right
+      this.right = this.arr.length - 1;
+
+    } else {
+      this.tail = this.right;
+      this.right -= 1
+
+    }
+    return x;
+  }
+
+  popFront() {
+    if (this.isEmpty()) {
+      return
+    }
+
     let x = this.arr[this.head];
 
-    if (this.head === this.arr.length - 1) {
+    if (this.left === this.arr.length - 1) {
       this.head = 1;
-    } else {
-      this.head += 1;
-    }
+      this.left = 0;
 
-    if (this.head === this.tail) {
-      this.head = 0;
-      this.tail = 1
+    } else {
+      this.head = this.left;
+      this.left += 1;
+
     }
     return x;
   }
 }
 
-let queue = new Queue();
-queue.enqueue(4);
-queue.enqueue(1);
-queue.enqueue(3);
-queue.dequeue();
-queue.enqueue(8);
-queue.dequeue();
-console.log(queue);
+let deque = new Deque();
+deque.pushFront(44)
+deque.pushBack(21);
+
+deque.pushFront(4);
+deque.pushBack(11);
+
+console.log("popBack", deque.popBack())
+deque.pushBack(20001);
+
+console.log("popFront", deque.popFront())
+deque.pushFront(20001);
+
+console.log(deque);
